502eb2ebd27f8e0325fa82e6654c6431971364d7
ilhamrecca <ilhamrecca@gmail.com>
2023-10-23 14:49:51 +0800

    fix mysql2

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 require("dotenv").config();
 const cors = require("cors");
 
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 4000;
 const errorHandler = require("./middlewares/errorHandlers");
 const retensisRoutes = require("./routes/retensisRoutes");
 
